55de3e667cddc2e01cca1125e4ae41a0daa1146c
Ahmed Osama <Ahmedosama8272@gmail.com>
2023-06-03 16:46:25 +0300

    #Day_1

---
+++ #Day_1/#Day_1.py
@@ -0,0 +1,13 @@
+####### Day_1 #######
+print("Hello World!")
+
+## \n new line 
+print("Hello World\nHello World")
+
+##Concatinating
+## use + to concatinate between two strings
+print("Hello " + "Ahmed")
+
+# In Python, indentation error is when there is an  
+# existing space before the code
+
