8b2d076f425556537260b6d4eab4a7782e8f221b
Ahmed Osama <Ahmedosama8272@gmail.com>
2023-06-03 17:55:07 +0300

    #Day_1

--- #Day_1/#Day_1.py
+++ #Day_1/#Day_1.py
@@ -4,12 +4,21 @@ print("Hello World!")
 ## \n new line 
 print("Hello World\nHello World")
 
-##Concatinating
+####### Concatinating #######
 ## use + to concatinate between two strings
 print("Hello " + "Ahmed")
 
 # In Python, indentation error is when there is an  
 # existing space before the code line
 
-#len ===> used to calculate the length of a string
+####### len ===> used to calculate the length of a string
 print(len(input("What is your name? ")))
+
+####### Python Variables #######
+## No type is assigned to variables, the variable type determined automatically when
+## assigning a value to it
+
+####### Python is a case-sensitive programming language
+
+name = input("Whta is your name? ")
+print(name)
