4bcdfcc10da13c884592233149a48ea893926d10
Ahmed Osama <Ahmedosama8272@gmail.com>
2023-06-17 15:42:11 +0300

    #Day_5

---
+++ #Day_5/#Day_5.py
@@ -0,0 +1,14 @@
+#Day_5
+# Loops in python
+# For Loops
+fruits = ["Apple","Peach","Pear"]
+for fruit in fruits: ## first time fruit = Apple , second time fruit  = Peach and so on
+  print(fruit)
+  print(fruit + " Pie")
+
+# For Loop with range(a,b,step) step is the incremental amount
+total = 0
+for number in range(1,101): ## Last number not taken into consideration
+  total += number
+
+print(total)
