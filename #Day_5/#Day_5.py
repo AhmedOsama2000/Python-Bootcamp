7e229473e4820fc1bd7946953dc00f7065105853
Ahmed Osama <Ahmedosama8272@gmail.com>
2023-07-22 15:42:05 +0300

    Modify Day_5 and add Add Day_6

--- #Day_5/#Day_5.py
+++ #Day_5/#Day_5.py
@@ -6,7 +6,7 @@ for fruit in fruits: ## first time fruit = Apple , second time fruit  = Peach an
   print(fruit)
   print(fruit + " Pie")
 
-# For Loop with range(a,b,step) step is the incremental amount
+# For Loop with range(a,b,step) step is the incremental amount (a ==> starts from 0 by default)
 total = 0
 for number in range(1,101): ## Last number not taken into consideration
   total += number
